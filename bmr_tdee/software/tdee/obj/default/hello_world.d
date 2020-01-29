1267a5ed3012c6a962d49d157cae6dab21bda650
朱峻平 <a5824384@gmail.com>
2020-01-29 21:56:51 +0800

    bmr

---
+++ bmr_tdee/software/tdee/obj/default/hello_world.d
@@ -0,0 +1,13 @@
+obj/default/hello_world.o: hello_world.c ../tdee_bsp/system.h \
+  ../tdee_bsp/linker.h ../tdee_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../tdee_bsp//HAL/inc/io.h ../tdee_bsp//HAL/inc/alt_types.h
+
+../tdee_bsp/system.h:
+
+../tdee_bsp/linker.h:
+
+../tdee_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../tdee_bsp//HAL/inc/io.h:
+
+../tdee_bsp//HAL/inc/alt_types.h:
